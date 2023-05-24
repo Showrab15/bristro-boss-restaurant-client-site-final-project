8ddca9e0f8f625ba4140691a15a4a648e72a0c43
Showrab15 <supto50showrab@gmail.com>
2023-05-25 03:04:33 +0600

    shop added

--- src/Pages/Order/Order.jsx
+++ src/Pages/Order/Order.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cover from '../Shared/Cover/Cover';
 import orderImg from '../../assets/order/order.jpg'
+
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import useMenu from '../../hooks/UseMenu';
+
 const Order = () => {
+
+    const [tabIndex , setTabIndex] = useState(0);
+    const [menu] = useMenu();
+    const deserts = menu.filter(item => item.category === 'dessert')
+const pizzas = menu.filter(item => item.category === 'pizza')
+const soups = menu.filter(item => item.category === 'soup')
+const salads = menu.filter(item => item.category === 'salad')
+const offer = menu.filter(item => item.category === 'offered')
     return (
         <div>
             <Cover
             img={orderImg}
             title="Order Food"
             ></Cover>
+          <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+      <TabList>
+        <Tab>Salad</Tab>
+        <Tab>Pizza</Tab>
+        <Tab>Soup</Tab>
+        <Tab>Desert</Tab>
+        <Tab>Drinks</Tab>
+      </TabList>
+      <TabPanel></TabPanel>
+      <TabPanel></TabPanel>
+    </Tabs>
         </div>
     );
 };
---
+++ src/components/FoodCard/FoodCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FoodCard = ({item}) => {
+    const {name, image, price , recipe} = item;
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default FoodCard;
